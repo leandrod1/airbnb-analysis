401ccc36f4e65c4aa0fb39a0fc4ebf3a5ad34d2c
Leandro Soares <133924248+leandrod1@users.noreply.github.com>
2026-09-03 18:21:40 +0000

    Refactor SQL query for clarity and correctness

--- revenue-per-host-with-notes.sql
+++ revenue-per-host-with-notes.sql
@@ -1,16 +1,16 @@
-SELECT host_id,host_name,
-    # Because some host has more that 1 accommodation, I want to know the number of accommodations per host
-    COUNT(host_name) as number_of_houses,
-    
-    # Because the price column has the the dollar sign in front of the number making it a string, we replace it
-    # We do the same for the comma ',' for number that are in the thousands, then we calculate the revenue in the last 30 days for every host
-    SUM(CAST(REPLACE(REPLACE (price, '$', ''),',','') AS UNSIGNED) * (30 - availability_30)) AS proj_rev_30,
-    
+SELECT host_id, host_name,
+    # Because some hosts have more than 1 accommodation, I want to know the number of accommodations per host
+    COUNT(host_name) AS number_of_houses,
+
+    # Because the price column has the dollar sign in front of the number, making it a string, we replace it
+    # We do the same for the comma ',' for numbers that are in the thousands, then we calculate the revenue in the last 30 days for every host
+    SUM(CAST(REPLACE(REPLACE(price, '$', ''), ',', '') AS UNSIGNED) * (30 - availability_30)) AS proj_rev_30,
+
     # We do the same to calculate the average revenue for every host
-    AVG(CAST(REPLACE(REPLACE (price, '$', ''),',','') AS UNSIGNED) * (30 - availability_30)) AS proj_avg_30
-    
+    AVG(CAST(REPLACE(REPLACE(price, '$', ''), ',', '') AS UNSIGNED) * (30 - availability_30)) AS proj_avg_30
+
 FROM airbnb.listings
-    
+
 # I want to know only for the accommodations that had availability in the last 365 days
 WHERE availability_365 > 0
 
